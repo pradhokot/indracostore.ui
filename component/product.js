705fb43cbed69fd0a87775e225806acde3665228
pradhokot <99374274+pradhokot@users.noreply.github.com>
2023-07-12 17:04:56 +0700

    add main wrapper

---
+++ component/product.js
@@ -0,0 +1,34 @@
+document.write(`
+<div class="card rounded-0 border-0 bg-light text-reset h-100">
+    <div class="card-header bg-transparent rounded-0 border-0 p-0">
+        <a class="text-reset text-decoration-none" href="#">
+            <div class="ratio ratio-1x1">
+                <img src="assets/img/no_images.svg" class="img-fluid w-100 opacity-50 no-images" style="background-color: #d1d2d2;" alt="">
+                <div>
+                    <img src="assets/img/product/produk.png" class="img-fluid w-100 bg-light" alt="">
+                </div>
+            </div>
+        </a>
+    </div>
+    <div class="card-body text-center lh-sm">
+        <p class="card-text small mb-1">
+            Kopi
+        </p>
+        <h5 class="card-title fs-reset fw-medium mb-0">
+            UCAFÉ Kopi Instan Hazelnut R10
+        </h5>
+        <p class="card-text small my-3">
+            Renceng (10 sachets x 20g)
+        </p>
+        <div class="card-text">
+            <strong>Rp 12.300,-</strong>
+        </div>
+    </div>
+    <div class="card-footer p-0 border-0">
+        <button class="btn btn-dark rounded-0 w-100">BELI</button>
+        <!--
+        <button class="btn btn-dark rounded-0 w-100" disabled>STOK HABIS</button>
+        -->
+    </div>
+</div>
+`)
