3e6e53aede9c1851d01fc351024e268c016ad1a7
pradhokot <99374274+pradhokot@users.noreply.github.com>
2023-08-10 16:36:55 +0700

    add card product

---
+++ component/card_produk.js
@@ -0,0 +1,27 @@
+document.write(`
+	<div class="card card-produk rounded-0 border-0 text-reset text-center h-100">
+	    <a class="text-decoration-none text-reset d-inline bg-light" href="#" style="flex-grow: 0;">
+	        <div class="ratio ratio-1x1">
+	            <img src="assets/img/no_images.svg">
+	            <img src="assets/img/product/produk.png" class="bg-light" alt="...">
+	        </div>
+	    </a>
+	    <div class="card-body bg-light d-flex flex-column" style="flex: 1 1 auto;">
+	        <div style="flex: 1 1 auto;">
+	            <h3 class="card-title fs-reset mb-1 opacity-75"><small>kopi</small></h3>
+	            <h4 class="card-title fs-reset mb-0 fw-medium">UCAFÉ Kopi Instan Cappuccino R10</h4>
+	            <p class="card-text my-2"><small>Renceng (10 sachets x 25g)</small></p>
+	        </div>
+	        <div class="harga-produk" style="flex-grow: 0;">
+	            <!-- <div class="harga-normal fw-medium">Rp 14.700,-</div> -->
+	            <div class="harga-promo">
+	                <span class="harga-reguler opacity-75"><del>Rp 14.700,-</del></span>
+	                <span class="harga-diskon fw-medium">Rp 13.500,-</span>
+	            </div>
+	        </div>
+	    </div>
+	    <div class="card-footer p-0 border-0 rounded-0">
+	        <a class="btn btn-dark w-100 rounded-0" href="#">BELI</a>
+	    </div>
+	</div>
+	`)
