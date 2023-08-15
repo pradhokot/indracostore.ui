aed0995fcf264b33002165931bfcbcf2d50887a4
pradhokot <99374274+pradhokot@users.noreply.github.com>
2023-08-15 16:59:58 +0700

    develop struktur dasar navigasi  bar

--- component/trigger_sidebar_filter.js
+++ component/trigger_sidebar_filter.js
@@ -1,5 +1,5 @@
 document.write(`
-	<div class="bg-white d-lg-none" style="position: -webkit-sticky; position: sticky; top: 41.6px; z-index: 1020; margin: .75rem 0; padding: .75rem 0;">
+	<div class="bg-white d-lg-none" style="position: -webkit-sticky; position: sticky; top: 41.6px; z-index: 1010; margin: .75rem 0; padding: .75rem 0;">
 		<div class="container">
 			<div class="row gx-2">
 				<div class="col">
